a480995a70d212df6eb58ad18f64de40a2bd675a
EndrewSilva <endrew.developer@gmail.com>
2025-01-09 15:13:38 -0300

    erro html with python

--- quotes/urls.py
+++ quotes/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path(
-        '', views.home, name='pagina-inicila',
-    )
+        '', views.home, name='pagina-inicila'
+    ),
+    path(
+        'brand/', views.brand, name='brands'
+    ),
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -5,7 +5,7 @@ from .services.api_clients.client import brand_consultation
 def home(request):
     return render(request, "home.html")
 
-def marcas(request):
+def brand(request):
     vehicle_type = request.POST.get('veiculo')
     response = brand_consultation(vehicle_type=vehicle_type)
     return render(request, "brand.html", {"vehicle_type": vehicle_type, "list_brand": response})
