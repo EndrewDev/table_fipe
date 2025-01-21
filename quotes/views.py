c669aef18aa939fefa278db5edbcb6ab32483508
EndrewSilva <endrew.developer@gmail.com>
2025-01-21 19:01:59 -0300

    Finally

--- quotes/views.py
+++ quotes/views.py
@@ -14,13 +14,13 @@ def brand(request):
         )
 
 def models(request):
-    vehicle_type = request.POST.get("tipo_veiculo")
+    vehicle_type = request.POST.get("vehicle_type")
     id_brand = request.POST.get("id_marca")
     name_brand = request.POST.get("nome_marca")
 
     response = models_consultation(
         vehicle_type=vehicle_type, id_brand=id_brand
-        )
+        ).get('modelos')
 
     return render(
         request, "models.html", {"list_models": response, "vehicle_type": vehicle_type, "id_brand": id_brand, "name_brand": name_brand}
@@ -30,14 +30,14 @@ def models_years(request):
     vehicle_type = request.POST.get("tipo_veiculo")
     id_brand = request.POST.get("id_marca")
     name_model = request.POST.get("nome_modelo")
-    id_model = request.POST.get("id_modelo")
+    id_models = request.POST.get("id_modelo")
 
     response = years_models_constultation(
-        vehicle_type=vehicle_type, id_brand=id_brand, id_model=id_model
+        vehicle_type=vehicle_type, id_brand=id_brand, id_models=id_models
         )
 
     return render(
-        request, "years_model.html", {"vehicle_type": vehicle_type, "id_brand": id_brand, "name_model": name_model, "id_model": id_model, "list_years_model": response}
+        request, "years_model.html", {"vehicle_type": vehicle_type, "id_brand": id_brand, "name_model": name_model, "id_model": id_models, "list_years_model": response}
         )
 
 def model_year_value(request):
